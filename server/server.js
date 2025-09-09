0c645bc9884cd055c6fc6cb0a826d31db0ecd864
binod rai <br163406@gmail.com>
2025-09-09 22:13:03 +0545

    install socket io

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
 const server = require("./src/app");
+const ConnectToDB = require("./src/db/db");
 
+
+
+ConnectToDB();
 server.listen(3000, () => {
-  console.log("Server listening on port 3001");
+  console.log("Server listening on port 3000");
+
 });
--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
-function connectDB() {
-       
+function ConnectToDB() {
+    mongoose.connect(process.env.DB_URL)
+    .then(() =>{
+        console.log("Connected to the DB");
+    })
+    .catch((err) =>{
+        console.log("Error connecting to the database", err);
+    });
 }
+
+module.exports = ConnectToDB;
